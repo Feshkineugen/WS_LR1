943a3282445e4452938a060cc8d09167e0ba23c1
Feshkineugen <118047190+Feshkineugen@users.noreply.github.com>
2024-04-03 11:06:25 +0300

    Add files via upload

---
+++ WS/WS/Program.cs
@@ -0,0 +1,15 @@
+﻿//Фешкин Евгений группа 22ит35 вариант 16  найти ((x-1)^2n+1)/(2n+1)((x+1)^2n+1
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double N = Convert.ToDouble(Console.ReadLine());
+        double x = Convert.ToDouble(Console.ReadLine());
+        double result = 2;
+        for(int i = 0;i<=N;i++)
+        {
+            result += (Math.Pow(x-1, 2*i + 1)) / ((2 * i + 1) * Math.Pow(x+1, 2 * i + 1));
+        }
+        Console.WriteLine(result);
+    }
+}
