c643792bb4377557c02f083ca9bc491547439ee0
Feshkineugen <118047190+Feshkineugen@users.noreply.github.com>
2024-04-03 11:18:48 +0300

    Update Program.cs

--- WS/WS/Program.cs
+++ WS/WS/Program.cs
@@ -6,10 +6,17 @@ internal class Program
         double N = Convert.ToDouble(Console.ReadLine());
         double x = Convert.ToDouble(Console.ReadLine());
         double result = 2;
+        if (Math.Abs(x) > 0)
+{
         for(int i = 0;i<=N;i++)
         {
             result += (Math.Pow(x-1, 2*i + 1)) / ((2 * i + 1) * Math.Pow(x+1, 2 * i + 1));
         }
         Console.WriteLine(result);
+        }
+        else
+{
+    Console.WriteLine("Значение X выходит за предел");
+}
     }
 }
